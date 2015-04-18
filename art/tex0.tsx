9295ae61ea02337a7d0ab25ed075585049c26efa
Martin Sandgren <martin@carlmartus.se>
2015-04-19 01:23:12 +0200

    Add: Player magic animation.

---
+++ art/tex0.tsx
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tex0" tilewidth="16" tileheight="16">
+ <image source="../www/tex0.png" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="libFloor0" tile="0"/>
+  <terrain name="libWall0" tile="5"/>
+  <terrain name="libwall1" tile="53"/>
+  <terrain name="libFloor1" tile="48"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,0,0,1"/>
+ <tile id="3" terrain="0,0,1,1"/>
+ <tile id="4" terrain="0,0,1,0"/>
+ <tile id="5" terrain="1,1,1,1"/>
+ <tile id="16" terrain="1,1,1,0"/>
+ <tile id="17" terrain="1,1,0,1"/>
+ <tile id="18" terrain="0,1,0,1"/>
+ <tile id="20" terrain="1,0,1,0"/>
+ <tile id="32" terrain="1,0,1,1"/>
+ <tile id="33" terrain="0,1,1,1"/>
+ <tile id="34" terrain="0,1,0,0"/>
+ <tile id="35" terrain="1,1,0,0"/>
+ <tile id="36" terrain="1,0,0,0"/>
+ <tile id="48" terrain="3,3,3,3"/>
+ <tile id="50" terrain="3,3,3,2"/>
+ <tile id="51" terrain="3,3,2,2"/>
+ <tile id="52" terrain="3,3,2,3"/>
+ <tile id="53" terrain="2,2,2,2"/>
+ <tile id="64" terrain="2,2,2,3"/>
+ <tile id="65" terrain="2,2,3,2"/>
+ <tile id="66" terrain="3,2,3,2"/>
+ <tile id="68" terrain="2,3,2,3"/>
+ <tile id="80" terrain="2,3,2,2"/>
+ <tile id="81" terrain="3,2,2,2"/>
+ <tile id="82" terrain="3,2,3,3"/>
+ <tile id="83" terrain="2,2,3,3"/>
+ <tile id="84" terrain="2,3,3,3"/>
+</tileset>

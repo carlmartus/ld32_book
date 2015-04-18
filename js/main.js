246a46a7c133a4a27d48b5a485d95060b8a4ebe1
Martin Sandgren <martin@carlmartus.se>
2015-04-18 10:59:32 +0200

    Add: GL frame.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ var gl;
 var screenW = 400, screenH = 300;
 
 function main() {
+	gl = esInitGl('bookCanvas');
+
+	gl.clearColor(0.3, 0.4, 0.5, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
 	console.log('Main');
 }
 

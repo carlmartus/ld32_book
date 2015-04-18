82c51961d3cd7cc861ceae42a7a773f4eaf06148
Martin Sandgren <martin@carlmartus.se>
2015-04-18 12:10:56 +0200

    Add: Load map data.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 var gl;
 var screenW = 400, screenH = 300;
 var blockRender = false;
+var tex0;
 
 function frame(ft) {
 	if (blockRender || ft > 0.3) return;
 
 	gl.clearColor(0.3, 0.4, 0.5, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
+
+function downloaded() {
+	console.log('Downloaded!');
+	esNextFrame(frame);
+
+	mapsLoad('tex0');
 }
 
 function main() {
@@ -19,6 +27,8 @@ function main() {
 		blockRender = true;
 	};
 
-	esNextFrame(frame);
+	var lod = new esLoad();
+	tex0 = lod.loadTexture(gl, 'tex0.png', gl.NEAREST, gl.LINEAR);
+	lod.downloadWithGlScreen(gl, downloaded);
 }
 
